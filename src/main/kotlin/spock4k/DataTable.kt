f38e0c28bcddc5ed06ea67567b454f3b63afa5e7
Rob Fletcher <rfletcher@netflix.com>
2017-11-06 09:35:03 -0800

    crude data table prototype

---
+++ src/main/kotlin/spock4k/DataTable.kt
@@ -0,0 +1,71 @@
+package spock4k
+
+import org.funktionale.partials.partially1
+
+object DataTableTest {
+  val `square ints` =
+    test { n: Int, expected: Int ->
+      println("$n * $n == $expected")
+      assert(n * n == expected)
+    } where {
+      listOf(
+        2 col 4,
+        4 col 16
+      )
+    }
+
+  val `multiply ints` =
+    test { n1: Int, n2: Int, expected: Int ->
+      println("$n1 * $n2 == $expected")
+      assert(n1 * n2 == expected)
+    } where {
+      listOf(
+        2 col 4 col 8,
+        4 col 2 col 8
+      )
+    }
+}
+
+fun <P1, P2> test(body: (P1, P2) -> Unit): ParametrizedTest2<P1, P2> =
+  ParametrizedTest2(body)
+
+infix fun <P1, P2> ParametrizedTest2<P1, P2>.where(
+  table: () -> Iterable<Pair<P1, P2>>
+): Iterable<() -> Unit> =
+  table()
+    .map { row ->
+      this@where
+        .partially1(row.first)
+        .partially1(row.second)
+    }
+
+class ParametrizedTest2<in P1, in P2>(body: (P1, P2) -> Unit)
+  : (P1, P2) -> Unit by body
+
+infix fun <P1, P2> P1.col(rhs: P2): Pair<P1, P2> =
+  Pair(this, rhs)
+
+fun <P1, P2, P3> test(body: (P1, P2, P3) -> Unit): ParametrizedTest3<P1, P2, P3> =
+  ParametrizedTest3(body)
+
+infix fun <P1, P2, P3> ParametrizedTest3<P1, P2, P3>.where(
+  table: () -> Iterable<Triple<P1, P2, P3>>
+): Iterable<() -> Unit> =
+  table()
+    .map { row ->
+      this@where
+        .partially1(row.first)
+        .partially1(row.second)
+        .partially1(row.third)
+    }
+
+class ParametrizedTest3<in P1, in P2, in P3>(body: (P1, P2, P3) -> Unit)
+  : (P1, P2, P3) -> Unit by body
+
+infix fun <P1, P2, P3> Pair<P1, P2>.col(rhs: P3): Triple<P1, P2, P3> =
+  Triple(this.first, this.second, rhs)
+
+fun main(args: Array<String>) {
+  DataTableTest.`square ints`.forEach { it() }
+  DataTableTest.`multiply ints`.forEach { it() }
+}
